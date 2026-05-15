c0c286582e2f3ba5866dc2f1e9531b9d65122c2e
chiken177@naver.com <alsgur3161@naver.com>
2026-05-15 11:50:05 +0900

    refactor: ReviewLikeResponse Kotlin 전환

---
+++ src/main/java/com/example/domain/reviewlike/dto/response/ReviewLikeResponse.kt
@@ -0,0 +1,8 @@
+package com.example.domain.reviewlike.dto.response
+
+data class ReviewLikeResponse(
+    val reviewId: Long?,
+    val memberId: Long?,
+    val isLiked: Boolean,
+    val likeCount: Int?
+)
