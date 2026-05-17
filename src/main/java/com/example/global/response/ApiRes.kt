b94aff1be536e45f74475c8625e51f53a32da9b8
jyeoniop <wldud128iop@gmail.com>
2026-05-17 20:46:35 +0900

    refactor: migrate global response ApiRes to Kotlin

--- src/main/java/com/example/global/response/ApiRes.kt
+++ src/main/java/com/example/global/response/ApiRes.kt
@@ -1,20 +1,7 @@
-package com.example.global.response;
+package com.example.global.response
 
-import lombok.Getter;
-
-@Getter
-public class ApiRes<T> {
-
-    private final int status;
-    private final String message;
-    private final T data;
-
-    public ApiRes(int status, String message, T data){
-        this.status = status;
-        this.message = message;
-        this.data = data;
-    }
-
-
-
-}
+class ApiRes<T>(
+    val status: Int,
+    val message: String,
+    val data: T?
+)
